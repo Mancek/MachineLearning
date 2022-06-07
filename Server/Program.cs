32a5d1506cb6d47a62cd02442c8eff5952afe07f
Mancek <52491015+Mancek@users.noreply.github.com>
2022-06-07 11:52:56 +0200

    Removed .idea folder

--- Server/Program.cs
+++ Server/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options => { options.Password.RequireNonAlphanumeric = false; });
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
